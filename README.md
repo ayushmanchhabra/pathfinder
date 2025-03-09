5b6198db84d8d79b0aeb988ed0e0373daabf0fb3
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-09 22:50:16 +0530

    Improve error handling

--- README.md
+++ README.md
@@ -8,13 +8,14 @@ This script (`pathfinder.sh`) tries to fingerprint the web server, brute forces 
 
 ## Prerequisites
 
-1. Ensure that awk, curl, dig and grep are installed.
+1. Ensure that awk, curl, dig, grep and nmap are installed.
 
 ## Usage
 
-1. Git clone this repo: git clone https://github.com/ayushmanchhabra/pathfinder
-1. Hop into the folder: cd ./pathfinder
-1. Execute the script: bash ./pathfinder.sh example.com
+1. Git clone this repo: `git clone https://github.com/ayushmanchhabra/pathfinder`
+1. Hop into the folder: `cd ./pathfinder`
+1. Make script executable: `chmod +x ./pathfinder.sh`
+1. Execute the script: `./pathfinder.sh example.com`
 
 ## License
 
--- pathfinder.sh
+++ pathfinder.sh
@@ -3,9 +3,18 @@
 echo -e "Legal disclaimer: Usage of pathfinder on hosts without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.\n"
 
 HOST="$1"
-IP_ADDR=$( dig +short "$HOST"  | paste -sd ", " - )
-SERVER=$( curl -sI "$HOST" | grep "Server:"  | awk '{print $2}' )
+if [[ -z "$HOST" ]]; then
+    echo "Expected a valid host, got nothing."
+    exit 1
+fi
 
+IP_ADDR=$( dig +short "$HOST"  | paste -sd ", " - )
+if [[ -z "$IP_ADDR" ]]; then
+    echo "Either received an invalid host or was unable to attain the IP address."
+    exit 1
+fi
+
+SERVER=$( curl -sI "$HOST" | grep "Server:"  | awk '{print $2}' )
 if [[ -z "$SERVER" ]]; then
     SERVER=$( nmap -p 443 -sV "$HOST" | grep "443" | awk '{print $4}' )
 else
